09f4841fe4b3be04316fa7c8cf1b246630400e65
gabak <gabmedia@yahoo.com>
2016-12-07 04:52:11 -0500

    any message

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gabak on 12/7/2016.
+ */
+
+var express = require('express'),
+    app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
